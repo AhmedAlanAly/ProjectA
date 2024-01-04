c049980dcebaf105f114a6ed5051019abff05f53
araslan <araslan@localhost>
2024-01-03 20:12:32 -0600

    Initial Commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,15 @@
+package a;
+
+public class DemoA {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Let's do it");
+		System.out.println("We can do it");
+
+		
+	}
+
+}
